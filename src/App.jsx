6f3f665b690a6d1d0a2f98afddabc66d3217676c
Anton <bodja.hrol12@gmail.com>
2025-07-24 16:40:08 +0300

    lp2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-wefrsdfewdf
-    </div>
+  <div className="App">
+        <header>
+          <img src={require('./images/Logo dark.svg').default} alt="" />
+              <div className='nav'>
+                <a href="">About Us</a>
+                <a href="">Marketplace</a>
+                <a href="">Accounts and Platforms</a>
+                <a href="">FAQ</a>
+            </div>
+            <div className='first_buttons'>
+                <button className='min_but'>
+                    Log in
+                </button>
+                <button className='black_button'>
+                    Sign up
+                </button>
+            </div>
+
+        </header>
+  </div>
   );
 }
 
