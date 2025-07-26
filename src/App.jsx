9e86a7ef46daa0ce2cc3a830c9a82a49719dcd63
Anton React <lykianovlav@gmail.com>
2025-07-26 19:23:49 +0300

    lp2

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ function App() {
   return (
   <div className="App">
         <header>
-          <img src={require('./images/Logo dark.svg').default} alt="" />
-              <div className='nav'>
-                <a href="">About Us</a>
-                <a href="">Marketplace</a>
-                <a href="">Accounts and Platforms</a>
-                <a href="">FAQ</a>
-            </div>
-            <div className='first_buttons'>
-                <button className='min_but'>
-                    Log in
-                </button>
-                <button className='black_button'>
-                    Sign up
-                </button>
+            <div className='header_section'>
+                    <img src={require('./images/Logo dark.svg').default} alt="" />
+                <div className='nav'>
+                    <a href="">About Us</a>
+                    <a href="">Marketplace</a>
+                    <a href="">Accounts and Platforms</a>
+                    <a href="">FAQ</a>
+                </div>
+                <div className='first_buttons'>
+                    <button className='min_but'>
+                        Log in
+                    </button>
+                    <button className='black_button'>
+                        Sign up
+                    </button>
+                </div>
             </div>
         </header>
         <div className='hero'>
@@ -34,21 +36,43 @@ function App() {
                     We provide you with a synthesis of the best features and convenient trading. Enjoy trading with our modernized platform.
                 </p>
                 <div className='buttons_first'>
-                    <button className='green_but'>
+                    <button className='green_but white'>
                         <span className='white'>
                             Start Trading
                         </span>
-                        <img src={require('./images/img_but_micro.svg').default} alt="" />
+                        <img className='img_arrow' src={require('./images/img_but_micro.svg').default} alt="" />
                     </button>
                     <button className='demo_btn'>
                         Try out the Demo
                     </button>
                 </div>
             </div>
-             <div className='img_group'>
-                <img src={require('./images/img_group.png')} alt="" />
+        </div>
+        <div className='img_group'>
+             <img className='img_group_wan' src={require('./images/img_group.png')} alt="" />
+        </div>
+        <div className='second_section'>
+            <h2>
+                Our partners include world market leaders
+            </h2>
+            <div className='img_group2'>
+                <img src={require('./images/binan_img.svg').default} alt="" />
+                <img src={require('./images/coin_img.svg').default} alt="" />
+                <img src={require('./images/ivest_img.svg').default} alt="" />
+                <img src={require('./images/bloc_img.svg').default} alt="" />
             </div>
         </div>
+        <div>
+            <div>
+                <h2>
+                    Unique platform for comfortable trading from anywhere in the world
+                </h2>
+                <p>
+                    COMPANY NAME is an award-winning platform that allows you to trade the world's financial markets.
+                </p>
+
+            </div>
+        </div>   
   </div>
   );
 }
